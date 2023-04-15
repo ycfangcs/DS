fc5caa5cdab7de53c6c9ddbd9d4026ebe3980be1
YuanchengFang <ridiculous46@outlook.com>
2023-04-15 12:21:44 +0800

    add avl

--- 04_Tree_part2/04_is_AVL.cpp
+++ 04_Tree_part2/04_is_AVL.cpp
@@ -49,6 +49,19 @@ TreeNode *createTree_not_blance() {
     return root;
 }
 
+/**
+ * O(n^2)的算法
+*/
+// 求一棵二叉树的深度
+int treeDepth(TreeNode *root)
+{
+    if (root == nullptr)
+        return 0;
+    int leftDepth = treeDepth(root->left);
+    int rightDepth = treeDepth(root->right);
+    return max(leftDepth, rightDepth) + 1;
+}
+
 // 判断一棵二叉树是否是平衡二叉树
 bool isAVL(TreeNode *root)
 {
@@ -59,4 +72,48 @@ bool isAVL(TreeNode *root)
     if (abs(leftDepth - rightDepth) > 1) // 如果左右子树深度相差大于1，则不是平衡二叉树
         return false;
     return isAVL(root->left) && isAVL(root->right); // 递归判断左右子树是否是平衡二叉树
+}
+
+/**
+ * O(n)的算法
+*/
+bool isAVL_improved(TreeNode *root, int &depth)
+{
+    if (root == nullptr)
+    {
+        depth = 0;
+        return true;
+    }
+    int leftDepth, rightDepth;
+    if (isAVL_improved(root->left, leftDepth) && isAVL_improved(root->right, rightDepth))
+    {
+        int diff = leftDepth - rightDepth;
+        if (diff <= 1 && diff >= -1)
+        {
+            depth = 1 + max(leftDepth, rightDepth);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+int main()
+{   
+    // 1. 构建一棵平衡二叉树
+    TreeNode *root = createTree_blance();
+    cout << "已构建一棵平衡二叉树:" << endl;
+    cout << "O(n^2)算法判断结果为: " << isAVL(root) << endl; // 1
+    int depth = 0;
+    cout << "O(n)算法判断结果为: " << isAVL_improved(root, depth) << endl; // 1
+
+    // 2. 构建一棵非平衡二叉树
+    root = createTree_not_blance();
+    cout << "已构建一棵非平衡二叉树:" << endl;
+    cout << "O(n^2)算法判断结果为: " << isAVL(root) << endl; // 0
+    depth = 0;
+    cout << "O(n)算法判断结果为: " << isAVL_improved(root, depth) << endl; // 0
+    
+    return 0;
 }
